d7996ec9470c0a2906082a1af130ff38fba07377
AndrewGerberBSU <66980113+AndrewGerberBSU@users.noreply.github.com>
2021-04-04 13:37:12 -0600

    worked on nfastate

--- p2/fa/nfa/NFAState.java
+++ p2/fa/nfa/NFAState.java
@@ -1,45 +1,81 @@
 package fa.nfa;
 
+import java.util.HashMap;
+import java.util.Set;
 import java.util.LinkedHashSet;
 
 public class NFAState extends fa.State {
 
-    private boolean isFinal;
+
+    //private vcariables
+    private boolean isFinal;//to hold if final
+
+    //Map of transitions on a character fir the given state
+    private HashMap<Character , LinkedHashSet<NFAState>> transitionMap;//based on fa.State and DFAState
+
+
 
 /*Contrsuctor
 * input: string name
 */
     public NFAState(String inputName){
+        this.name = inputName;
+        transitionMap = new HashMap<Character, LinkedHashSet<NFAState>>();
+        isFinal = false;
     }
-/*Contrsuctor
-* input: string name
-* input boolean
-*/
+    /*Contrsuctor
+    * input: string name
+    * input boolean
+    * Use: if needed to maike a state that you know is final rather than change to final
+    */
     public NFAState(String inputname, boolean bool){
-
+        this.name = inputname;
+        transitionMap = new HashMap<Character, LinkedHashSet<NFAState>>();
+        isFinal = bool;
     }
 
     public boolean isFinal(){
         return this.isFinal();
     }
-
-    public void addTransition(char onSymb, LinkedHashSet<NFAState> destination){
-
+    /**
+	 * Method to add a transition to the DFA
+	 * @param onSymb - symbol need to make transition to toState
+	 * @param toState - state that will be transitioned to
+	 */
+    //BASED ON DFA
+    public void addTransition(char onSymb, LinkedHashSet<NFAState> toState){
+        transitionMap.put(onSymb, toState);
     }
-
-    public void addTransition(char onSymb, NFAState destination){
-
-    }
-
-    public LinkedHashSet<NFAState> getTo(char onSymb){
-
-    }
-
-    public boolean hasTransition(char c){
-
-    }
-
-    public Set<NFAState> getTransition(char c){
+    /**
+	 * Method to add a transition to the DFA
+     *  IF there the transiton doesnt exist make transition else make branch transiton
+	 * @param onSymb - symbol need to make transition to toState
+	 * @param toState - state that will be transitioned to
+	 */
+    
+    public void addTransition(char onSymb, NFAState toState){
         
+        if(transitionMap.get(onSymb) == null){//conditon for it to be empty
+            //make single hashset
+            LinkedHashSet<NFAState> firstTrans = new LinkedHashSet<NFAState>();
+            
+            firstTrans.add(toState);
+            transitionMap.put(onSymb, firstTrans);
+        }
+        else{//conditon for it to not empty
+            //make branch linked hash
+            LinkedHashSet<NFAState> multiTrans = new LinkedHashSet<NFAState>();
+            multiTrans.add(toState);
+            transitionMap.put(onSymb, multiTrans);
+        }
     }
+    public String getNameNFA(){
+        return this.name;
+    }
+    //returns transtion from current state given on symbol
+    public LinkedHashSet<NFAState> getTrans(char onSymb){
+        return transitionMap.get(onSymb);
+    }
+
+    
 }
