0737fda2834de35de631ed52f939c94c13b1aa96
CesarRaymundo <cesarraymundo@u.boisestate.edu>
2021-04-05 15:37:21 -0600

    Finished some methods

--- p2/fa/nfa/NFA.java
+++ p2/fa/nfa/NFA.java
@@ -4,43 +4,161 @@ import java.util.Set;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.HashSet;
+import java.util.Iterator;
 import fa.State;
 import fa.dfa.DFA;
 
 public class NFA implements NFAInterface{
+    
+    private NFAState finalState; //final state
     private NFAState startState; //starting state
+    private LinkedHashSet<NFAState> eClosure;// holds the states that can be transtioned with 'e'
     private LinkedHashSet<NFAState> states; //holds all the states of the NFA	
-	private HashSet<Character> alphabet; //includes all letters used
+	private HashSet<Character> sigma; //includes all letters used
     
     public NFA(){
 		states = new LinkedHashSet<>();
-		alphabet = new HashSet<>();
+		sigma = new HashSet<>();
     }
   
     @Override
     public void addStartState(String name) {
-        // TODO Auto-generated method stub
+        startState = new NFAState(name);
+        if(startState != null){
+            checkState(startState);
+        }else{
+            addState(name);
+        }
         
     }
 
     @Override
     public void addState(String name) {
-        // TODO Auto-generated method stub
-        
+       NFAState stateToAdd = new NFAState(name);
+       checkState(stateToAdd);
     }
 
     @Override
     public void addFinalState(String name) {
-        // TODO Auto-generated method stub
+        finalState = new NFAState(name,true);
+        checkState(finalState);
         
     }
 
     @Override
     public void addTransition(String fromState, char onSymb, String toState) {
-        // TODO Auto-generated method stub
-        
-    }
+        if(!sigma.contains(onSymb)) {
+			sigma.add(onSymb);
+		}
+		Iterator<NFAState> toItr = states.iterator();
+		Iterator<NFAState> fromItr = states.iterator();
+		NFAState fromS = fromItr.next();
+		while(fromItr.hasNext() 
+				&& !fromS.getNameNFA().equals(fromState)) {
+			fromS = fromItr.next();
+		}
 
+		NFAState toS = toItr.next();
+		while(toItr.hasNext() 
+				&& !toS.getNameNFA().equals(toState)) {
+			toS = toItr.next();
+		}
+		fromS.addTransition(onSymb, toS);
+		
+        if(!sigma.contains(onSymb) && onSymb != 'e'){
+            sigma.add(onSymb);
+        }
+	}  
+    
+	/**
+	 * Construct the textual representation of the DFA, for example
+	 * A simple two state DFA
+	 * Q = { a b }
+	 * Sigma = { 0 1 }
+	 * delta =
+	 *		0	1	
+	 *	a	a	b	
+	 *	b	a	b	
+	 * q0 = a
+	 * F = { b }
+	 * 
+	 * The order of the states and the alphabet is the order
+	 * in which they were instantiated in the DFA.
+	 * @return String representation of the DFA
+	 */
+	//public String toString() {
+		// String formatedString = "";
+		
+		// //printing the whole set of states
+		// formatedString += "Q = { ";
+		// Object[] statesArray  = states.toArray();
+		// Iterator<NFAState> its = states.iterator();
+
+		// while(its.hasNext()) {
+		// 	formatedString += its.next().getNameNFA() + " ";
+		// }
+		// formatedString += "} \n";
+		
+		// //printing alphabet
+		// formatedString += "Sigma = { ";
+		// Object[] sigmaArray = sigma.toArray();
+		// for (int i = 0; i < sigmaArray.length; i++) {
+		// 	formatedString += sigmaArray[i] + " ";
+		// }
+		// formatedString += "} \n";
+		
+		
+		
+		
+		// //printing delta
+		// formatedString += "delta =  \n\t\t";
+		// //for every character in alphabet
+		// for (int i = 0; i < sigmaArray.length; i++) { 
+		// 	formatedString += sigmaArray[i] + " \t";
+			
+		// }
+		// formatedString += "\n\t";
+		
+		
+		// for (int j = 0; j < statesArray.length; j++) { //for every state
+		// 	formatedString += ((NFAState)statesArray[j]).getNameNFA() + "\t";
+		// 	for (int i = 0; i < sigmaArray.length; i++) {
+		// 		//adds the resulting state of the transition to the output string
+		// 		formatedString += getToState((NFASState)statesArray[j], (Character)alphabetArray[i]).getNameDFA(); 
+		// 		formatedString += " \t";
+		// 	}
+			
+			
+		// 	//get transition given state and character
+			
+		// 	formatedString += "\n\t"; //go onto next state
+		// }
+		
+		
+		
+		
+		
+		
+		// formatedString += "\n";
+		
+		// //printing start state
+		// formatedString += "q0 = " + startState.getNameDFA() + "\n";
+		
+		// //printing final state
+		// formatedString += "F = { ";
+
+		
+		// Iterator<NFAtate> itf = finalStates.iterator();
+		// while(itf.hasNext()) {
+		// 	formatedString += itf.next().getNameDFA() + " ";
+		// }
+		
+		// formatedString += "} \n";
+		
+		
+		
+		// return formatedString;
+//	}
     @Override
     public Set<? extends State> getStates() {
         return states;
@@ -48,8 +166,13 @@ public class NFA implements NFAInterface{
 
     @Override
     public Set<? extends State> getFinalStates() {
-        // TODO Auto-generated method stub
-        return null;
+        Set<NFAState> finalStates = new HashSet<NFAState>();
+        for (NFAState x: states){
+            if (x.isFinal()){
+                finalStates.add(x);
+            }
+        }
+        return finalStates;
     }
 
     @Override
@@ -59,7 +182,7 @@ public class NFA implements NFAInterface{
 
     @Override
     public Set<Character> getABC() {
-     return alphabet;
+     return sigma;
     }
 
     @Override
@@ -70,14 +193,35 @@ public class NFA implements NFAInterface{
 
     @Override
     public Set<NFAState> getToState(NFAState from, char onSymb) {
-        // TODO Auto-generated method stub
-        return null;
+        return from.getTrans(onSymb);
     }
 
     @Override
     public Set<NFAState> eClosure(NFAState s) {
-        // TODO Auto-generated method stub
-        return null;
+          if(!s.getTrans('e').equals(new HashSet<NFAState>())) {
+        	for (NFAState x : s.getTrans('e')) {
+                if (!this.eClosure.contains(x)) {
+                    this.eClosure.add(x);
+                    eClosure(x);
+                }
+            }
+        } else {
+        	this.eClosure.add(s);
+        }
+		return this.eClosure;
+    }
+    
+
+    /**
+     * Checks if the state already exists.
+     * Adds the state to the list if not already added.
+     * 
+     * @param x
+     */
+    private void checkState(NFAState x){
+        if(!states.contains(x)){
+            states.add(x);
+        }
     }
     
 }
