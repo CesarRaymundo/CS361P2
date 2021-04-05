e7d7719841ff986e0b8091501e92635cdd938244
CesarRaymundo <cesarraymundo@u.boisestate.edu>
2021-04-05 13:38:20 -0600

    Cleaned up

--- p2/fa/nfa/NFA.java
+++ p2/fa/nfa/NFA.java
@@ -1,125 +1,83 @@
 package fa.nfa;
 
 import java.util.Set;
-
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.HashSet;
 import fa.State;
 import fa.dfa.DFA;
 
 public class NFA implements NFAInterface{
-
-
-
+    private NFAState startState; //starting state
+    private LinkedHashSet<NFAState> states; //holds all the states of the NFA	
+	private HashSet<Character> alphabet; //includes all letters used
+    
     public NFA(){
-
+		states = new LinkedHashSet<>();
+		alphabet = new HashSet<>();
+    }
+  
+    @Override
+    public void addStartState(String name) {
+        // TODO Auto-generated method stub
+        
     }
 
-   /**
-	 * Adds the initial state to the DFA instance
-	 * @param name is the label of the start state
-	 */
-	public abstract void addStartState(String name){
-
+    @Override
+    public void addState(String name) {
+        // TODO Auto-generated method stub
+        
     }
 
-	/**
-	 * Adds a non-final, not initial state to the DFA instance
-	 * @param name is the label of the state 
-	 */
-	public abstract void addState(String name){
-
+    @Override
+    public void addFinalState(String name) {
+        // TODO Auto-generated method stub
+        
     }
 
-	/**
-	 * Adds a final state to the DFA
-	 * @param name is the label of the state
-	 */
-	public abstract void addFinalState(String name){
-
+    @Override
+    public void addTransition(String fromState, char onSymb, String toState) {
+        // TODO Auto-generated method stub
+        
     }
 
-
-	/**
-	 * Adds the transition to the DFA's delta data structure
-	 * @param fromState is the label of the state where the transition starts
-	 * @param onSymb is the symbol from the DFA's alphabet.
-	 * @param toState is the label of the state where the transition ends
-	 */
-	public abstract void addTransition(String fromState, char onSymb, String toState){
-
-            }
-	
-	/**
-	 * Getter for Q
-	 * @return a set of states that FA has
-	 */
-	public abstract Set<? extends State> getStates(){
-
-    }
-	
-	/**
-	 * Getter for F
-	 * @return a set of final states that FA has
-	 */
-	public abstract Set<? extends State> getFinalStates(){
-
-    }
-	
-	/**
-	 * Getter for q0
-	 * @return the start state of FA
-	 */
-	public abstract State getStartState(){
-
-    }
-	
-	/**
-	 * Getter for the alphabet Sigma
-	 * @return the alphabet of FA
-	 */
-	public abstract Set<Character> getABC(){
-
+    @Override
+    public Set<? extends State> getStates() {
+        return states;
     }
 
-	@Override
-	public Set<? extends State> getStates() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<? extends State> getFinalStates() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public State getStartState() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<Character> getABC() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public DFA getDFA() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<NFAState> getToState(NFAState from, char onSymb) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<NFAState> eClosure(NFAState s) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Set<? extends State> getFinalStates() {
+        // TODO Auto-generated method stub
+        return null;
     }
+
+    @Override
+    public State getStartState() {
+      return startState;
+    }
+
+    @Override
+    public Set<Character> getABC() {
+     return alphabet;
+    }
+
+    @Override
+    public DFA getDFA() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<NFAState> getToState(NFAState from, char onSymb) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<NFAState> eClosure(NFAState s) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
 }
